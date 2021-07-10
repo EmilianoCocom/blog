57faed969a52aa22b312454b48680793fc64712b
Emiliano Cocom <joseemilianococom@gmail.com>
2021-07-10 14:12:47 -0500

    Avance guardado del proyecto blog

---
+++ src/appblog/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //Relacion uno a muchos inversa
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function categories()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    //Relacion muchos a muchos
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag');
+    }
+
+    //Relacion uno a uno polimorfica
+    public function image()
+    {
+        return $this->morphOne('App\Models\Image', 'imageable');
+    }
+}
