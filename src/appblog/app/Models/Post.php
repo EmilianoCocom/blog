f4acf9fa113425eeb7deab937e1fe9d0bccafc55
Emiliano Cocom <joseemilianococom@gmail.com>
2021-08-26 18:58:46 -0500

    crud de categorias, etiquetas terminado, crud de posts en proceso

--- src/appblog/app/Models/Post.php
+++ src/appblog/app/Models/Post.php
@@ -9,13 +9,15 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     //Relacion uno a muchos inversa
     public function user()
     {
         return $this->belongsTo('App\Models\User');
     }
 
-    public function categories()
+    public function category()
     {
         return $this->belongsTo('App\Models\Category');
     }
